cc9a018ce0e07fb82401aeddd0dad3b32ca594bf
rdrgpaz <rdrgpaz@gmail.com>
2026-03-26 21:20:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Emporos is alive 🚀");
+
+  // rota principal
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    return res.end("Emporos is alive");
+  }
+
+  // rota de teste
+  if (req.url === "/generate") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify({
+      message: "Emporos pronto para gerar campanhas"
+    }));
+  }
+
+  // qualquer outra coisa
+  res.writeHead(404);
+  res.end("Not found");
+
 });
 
 const PORT = process.env.PORT || 3000;
